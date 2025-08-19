13ab7a89a3c7b838eb96280690e2f5f5f04642f7
sneha-verse <sneha.annamareddy@gmail.com>
2025-08-18 21:48:06 -0500

    Initial Spring setup

---
+++ src/test/java/com/sneha/ride/RideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sneha.ride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
